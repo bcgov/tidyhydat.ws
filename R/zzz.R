0cfd4d621cd7b937460c5b1e351c561994edc218
Sam Albers <sam.albers@gov.bc.ca>
2018-04-16 08:52:41 -0700

    using tidy-eval

--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,6 @@
-.onLoad <-
-  function(libname = find.package("tidyhydat"),
-           pkgname = "tidyhydat") {
-    # CRAN Note avoidance
-    if (getRversion() >= "2.15.1")
-      utils::globalVariables(
-        # Vars used in Non-Standard Evaluations, declare here to avoid CRAN warnings
-        ## This is getting ridiculous
-        c("ID",
-          "param_id",
-          "Name_Fr",
-          "STATION_NUMBER",
-          "Date",
-          "Name_En",
-          "Value",
-          "Unit",
-          "Grade",
-          "Symbol",
-          "Approval",
-          "Parameter",
-          "Code",
-          "." # piping requires '.' at times
-        )
-      )
-    invisible()
-  }
+#.onLoad <-
+#  function(libname = find.package("tidyhydat"),
+#           pkgname = "tidyhydat") {
+#   if(Sys.getenv(""WS_USRNM""))
+#  }
+#
