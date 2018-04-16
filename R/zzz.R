690a85623b00d6efc26871ad4541982d0a5d1341
Sam Albers <sam.albers@gov.bc.ca>
2018-04-16 09:12:22 -0700

    rename vignette, adding pkg load message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,9 @@
-#.onLoad <-
-#  function(libname = find.package("tidyhydat"),
-#           pkgname = "tidyhydat") {
-#   if(Sys.getenv(""WS_USRNM""))
-#  }
-#
+.onLoad <-
+ function(libname = find.package("tidyhydat"),
+          pkgname = "tidyhydat") {
+  if(!nzchar(Sys.getenv("WS_USRNM")) | !nzchar(Sys.getenv("WS_PWD"))){
+    packageStartupMessage("You do not have your webservice credentials setup. For directions please run:")
+    packageStartupMessage("vignette('setup_webservice_credentials', package = 'tidyhydat.ws')")
+  }
+ }
+
